6c4e864d58773b689965e6ef5204b0c0b019443a
LoveGlaze <617683180@qq.com>
2022-02-19 20:41:38 +0800

    添加压缩py

---
+++ zip.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 19 19:51:59 2022
+
+@author: LoveGlaze
+"""
+import zipfile
+f = zipfile.ZipFile('MacRecovery.zip','w', zipfile.ZIP_DEFLATED)
+f.write('macrecovery.exe')
+f.write('Mac恢复分区下载.bat')
+f.write('README.md')
+f.close()
