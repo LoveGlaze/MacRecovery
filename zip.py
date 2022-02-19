eee551c006ef8bb75d5f6695d2a52eae750ebd12
LoveGlaze <77913753+LoveGlaze@users.noreply.github.com>
2022-02-19 21:33:58 +0800

    Update zip.py

--- zip.py
+++ zip.py
@@ -6,7 +6,7 @@ Created on Sat Feb 19 19:51:59 2022
 """
 import zipfile
 f = zipfile.ZipFile('MacRecovery.zip','w', zipfile.ZIP_DEFLATED)
-f.write('macrecovery.exe')
+f.write('dist/macrecovery.exe')
 f.write('Mac恢复分区下载.bat')
 f.write('README.md')
 f.close()
